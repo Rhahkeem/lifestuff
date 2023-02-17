636c83c75686abb22883edd6162467d583d7fea2
Rhahkeem <d_xtreme93@hotmail.com>
2023-02-17 00:00:00 +0000

    Working command line for multi-options

--- src/main.rs
+++ src/main.rs
@@ -21,11 +21,26 @@ enum Commands {
 #[derive(Debug, Args)]
 struct Conversions {
     #[arg(value_enum)]
-    from: Units,
+    from: FromUnits,
+    value: f64,
+    // #[clap(visible_aliases=["a","acres"], long, action=clap::ArgAction::SetTrue, group="area")]
+    // to_acres: Option<bool>,
+    // #[clap(long, visible_aliases=["squarefeet","sqft"], action=clap::ArgAction::SetTrue, group="area")]
+    // to_sqft: Option<bool>,
+    // #[clap(visible_aliases=["metres","m"], long, action=clap::ArgAction::SetTrue, group="area")]
+    // to_metres: Option<bool>,
+    // #[clap( visible_aliases=["inches","i"],long, action=clap::ArgAction::SetTrue, group="area")]
+    // to_inches: Option<bool>,
+    // #[clap(long,visible_aliases=["mi","miles"], action=clap::ArgAction::SetTrue, group="area")]
+    // to_miles: Option<bool>,
+    // #[clap(long, visible_aliases=["km","kilometres"], action=clap::ArgAction::SetTrue, group="area")]
+    // to_kilometres: Option<bool>,
+    #[clap(long)]
+    to: Vec<ToUnits>,
 }
 
 #[derive(Subcommand, Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
-enum Units {
+enum FromUnits {
     /// Convert From Acres
     Acres,
     /// Convert From Square Feet
@@ -36,30 +51,27 @@ enum Units {
     Inches,
     /// Convert From Square Miles
     Miles,
-    /// Convert From Square Feet
+    /// Convert From Square Kilometres
     Kilometres,
 }
+
+#[derive(Subcommand, Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+enum ToUnits {
+    /// Convert To Acres
+    Acres,
+    /// Convert To Square Feet
+    Sqft,
+    /// Convert To Square Metres
+    Metres,
+    /// Convert To Square Inches
+    Inches,
+    /// Convert To Square Miles
+    Miles,
+    /// Convert To Square Kilometres
+    Kilometres,
+}
+
 // LOOKS LIKE ->>> prog convert {options} <val> --to{options}
-
-// #[derive(Debug, Args)]
-// struct FromUnits {
-//     #[clap(group = "FromArg")]
-//     acres: f64,
-//     sqft: f64,
-//     metres: f64,
-//     inches: f64,
-//     miles: f64,
-// }
-
-// #[derive(Debug, Args)]
-// struct ToUnits {
-//     #[clap(group = "FromArg")]
-//     acres: f64,
-//     sqft: f64,
-//     metres: f64,
-//     inches: f64,
-//     miles: f64,
-// }
 
 // prog convert from {options} <val> to {options}
 
@@ -67,20 +79,17 @@ fn main() {
     let cli = Cli::parse();
     match &cli.command {
         Commands::Convert(blah) => {
-            println!("Got blah {:?}", blah.from);
-            match &blah.from {
-                Units::Acres => println!("Got Acres!!!"),
-                Units::Inches => println!("Got Inches!!!"),
-                Units::Kilometres => println!("Got Kilometres!!!"),
-                Units::Metres => println!("Got Metres!!"),
-                Units::Miles => println!("Got Miles!!"),
-                Units::Sqft => println!("Got Squarefeet"),
-            }
+            println!("Got blah {:?}", blah);
+            // match &blah.from {
+            //     Units::Acres => println!("Got Acres!!!"),
+            //     Units::Inches => println!("Got Inches!!!"),
+            //     Units::Kilometres => println!("Got Kilometres!!!"),
+            //     Units::Metres => println!("Got Metres!!"),
+            //     Units::Miles => println!("Got Miles!!"),
+            //     Units::Sqft => println!("Got Squarefeet"),
+            // }
         }
     }
 }
 
-// acres to sq ft
-// metres to inches
-// km to miles
 // compound interest
