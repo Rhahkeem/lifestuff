e0c186c346dbcb9bfd53b093abfbb98dbff730c4
Rhahkeem <d_xtreme93@hotmail.com>
2023-02-18 18:11:54 +0000

    Parsing for area unit conversions
    This creates a working example for area conversions.
    Using area conversions can now be done

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::{Args, Parser, Subcommand, ValueEnum};
+use strum::Display;
 
 #[derive(Parser)]
 #[clap(
@@ -23,71 +24,113 @@ struct Conversions {
     #[arg(value_enum)]
     from: FromUnits,
     value: f64,
-    // #[clap(visible_aliases=["a","acres"], long, action=clap::ArgAction::SetTrue, group="area")]
-    // to_acres: Option<bool>,
-    // #[clap(long, visible_aliases=["squarefeet","sqft"], action=clap::ArgAction::SetTrue, group="area")]
-    // to_sqft: Option<bool>,
-    // #[clap(visible_aliases=["metres","m"], long, action=clap::ArgAction::SetTrue, group="area")]
-    // to_metres: Option<bool>,
-    // #[clap( visible_aliases=["inches","i"],long, action=clap::ArgAction::SetTrue, group="area")]
-    // to_inches: Option<bool>,
-    // #[clap(long,visible_aliases=["mi","miles"], action=clap::ArgAction::SetTrue, group="area")]
-    // to_miles: Option<bool>,
-    // #[clap(long, visible_aliases=["km","kilometres"], action=clap::ArgAction::SetTrue, group="area")]
-    // to_kilometres: Option<bool>,
-    #[clap(long)]
+    #[clap(long, required = true)]
     to: Vec<ToUnits>,
 }
 
-#[derive(Subcommand, Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+fn unit_conversions(from: &FromUnits, to: &ToUnits, val: &f64) -> f64 {
+    match &from {
+        FromUnits::Acres => match &to {
+            ToUnits::Acres => return val.to_owned(),
+            ToUnits::Inches => return val * 6272640.0,
+            ToUnits::Km => return val * 0.004047,
+            ToUnits::Metres => return val * 4046.856422,
+            ToUnits::Miles => return val / 640.0,
+            ToUnits::Sqft => return val * 43560.0,
+        },
+        FromUnits::Inches => match &to {
+            ToUnits::Acres => return val / 6272640.0,
+            ToUnits::Inches => val.to_owned(),
+            ToUnits::Km => return val * 0.00000000064516,
+            ToUnits::Metres => return val * 0.000645,
+            ToUnits::Miles => return val * 0.0000000002491,
+            ToUnits::Sqft => return val / 144.0,
+        },
+        FromUnits::Kilometres => match &to {
+            ToUnits::Acres => return val * 247.105381,
+            ToUnits::Inches => val * 1550003100.0062,
+            ToUnits::Km => return val.to_owned(),
+            ToUnits::Metres => return val * 1000000.0,
+            ToUnits::Miles => return val * 0.386102,
+            ToUnits::Sqft => return val * 10763910.41671,
+        },
+        FromUnits::Metres => match &to {
+            ToUnits::Acres => return val * 0.000247,
+            ToUnits::Inches => val * 1550003100.0062,
+            ToUnits::Km => return val / 1000000.0,
+            ToUnits::Metres => val.to_owned(),
+            ToUnits::Miles => return val / 0.0000003861,
+            ToUnits::Sqft => return val * 10.76391,
+        },
+        FromUnits::Miles => match &to {
+            ToUnits::Acres => return val * 640.0,
+            ToUnits::Inches => val * 4014489599.4792,
+            ToUnits::Km => return val * 2.589988,
+            ToUnits::Metres => val * 2589988.11,
+            ToUnits::Miles => return val.to_owned(),
+            ToUnits::Sqft => return val * 27878399.996383,
+        },
+        FromUnits::Sqft => match &to {
+            ToUnits::Acres => return val / 43560.0,
+            ToUnits::Inches => return val * 144.0,
+            ToUnits::Km => return val / 0.000000092903,
+            ToUnits::Metres => val * 0.092903,
+            ToUnits::Miles => return val / 0.00000003587,
+            ToUnits::Sqft => return val.to_owned(),
+        },
+    }
+}
+
+#[derive(Subcommand, Debug, Display, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
 enum FromUnits {
     /// Convert From Acres
     Acres,
-    /// Convert From Square Feet
-    Sqft,
-    /// Convert From Square Metres
-    Metres,
     /// Convert From Square Inches
     Inches,
-    /// Convert From Square Miles
-    Miles,
     /// Convert From Square Kilometres
     Kilometres,
+    /// Convert From Square Metres
+    Metres,
+    /// Convert From Square Miles
+    Miles,
+    /// Convert From Square Feet
+    Sqft,
 }
 
-#[derive(Subcommand, Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+#[derive(Subcommand, Debug, Display, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
 enum ToUnits {
+    #[clap(action=clap::ArgAction::SetTrue, alias = "a")]
     /// Convert To Acres
     Acres,
-    /// Convert To Square Feet
-    Sqft,
-    /// Convert To Square Metres
-    Metres,
+    #[clap(action=clap::ArgAction::SetTrue, aliases = ["i","in"])]
     /// Convert To Square Inches
     Inches,
+    #[clap(action=clap::ArgAction::SetTrue, alias = "kilometres")]
+    /// Convert To Square Kilometres
+    Km,
+    #[clap(action=clap::ArgAction::SetTrue, alias = "m")]
+    /// Convert To Square Metres
+    Metres,
+    #[clap(action=clap::ArgAction::SetTrue, alias = "mi")]
     /// Convert To Square Miles
     Miles,
-    /// Convert To Square Kilometres
-    Kilometres,
+    #[clap(action=clap::ArgAction::SetTrue, aliases = ["squarefeet","s"])]
+    /// Convert To Square Feet
+    Sqft,
 }
 
-// LOOKS LIKE ->>> prog convert {options} <val> --to{options}
-
-// prog convert from {options} <val> to {options}
+fn conversion_prep(from: &FromUnits, to: &Vec<ToUnits>, val: &f64) {
+    for unit in to {
+        let conversion = unit_conversions(from, unit, val);
+        println!("{val} {from} = {conversion} {unit}");
+    }
+}
 
 fn main() {
     let cli = Cli::parse();
     match &cli.command {
-        Commands::Convert(blah) => {
-            println!("Got blah {:?}", blah);
-            // match &blah.from {
-            //     Units::Acres => println!("Got Acres!!!"),
-            //     Units::Inches => println!("Got Inches!!!"),
-            //     Units::Kilometres => println!("Got Kilometres!!!"),
-            //     Units::Metres => println!("Got Metres!!"),
-            //     Units::Miles => println!("Got Miles!!"),
-            //     Units::Sqft => println!("Got Squarefeet"),
-            // }
+        Commands::Convert(args) => {
+            conversion_prep(&args.from, &args.to, &args.value);
         }
     }
 }
