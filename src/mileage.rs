b31c3af42308c5ea1099f706f484ebca8990b8fa
Rhahkeem <d_xtreme93@hotmail.com>
2024-05-06 14:39:50 +0100

    Added more output for mileage diff

--- src/mileage.rs
+++ src/mileage.rs
@@ -18,9 +18,8 @@ pub fn handle_mileage_operations(mileage_args: &Mileage, _: bool) -> Result<()> 
     let initial_mileage: f32 = 8300.0;
     let projected_mileage = initial_mileage + (num_days_since_start * mileage_per_day).ceil();
     let under_allowance = (mileage_args.mileage as f32) < projected_mileage;
-    let delta = ((projected_mileage - (mileage_args.mileage as f32)) / mileage_per_day)
-        .ceil()
-        .abs();
-    println!("Current mileage is {}, projected mileage is {projected_mileage}. Current mileage is {} by {delta} days ", mileage_args.mileage, if under_allowance {"under".italic().green() }else{"over".bold().red() },  );
+    let mileage_delta = (projected_mileage - (mileage_args.mileage as f32)).abs();
+    let daily_delta = (mileage_delta / mileage_per_day).ceil().abs();
+    println!("Current mileage is {}, projected mileage is {projected_mileage}. Current mileage is {} by {daily_delta} days or {mileage_delta} miles", mileage_args.mileage, if under_allowance {"under".italic().green() }else{"over".bold().red() },  );
     Ok(())
 }
